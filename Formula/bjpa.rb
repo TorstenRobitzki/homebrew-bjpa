47393c2becfe84e7cc4d17c48d2b4d0f1d9728b3
Torsten Robitzki <Torsten@Robitzki.de>
2019-06-28 16:59:35 +0200

    asd

--- Formula/bjpa.rb
+++ Formula/bjpa.rb
@@ -3,7 +3,7 @@ require 'fileutils'
 class Bjpa < Formula
   desc "Tools for BJPA"
   homepage "http://Torrox.de"
-  url "https://github.com/TorstenRobitzki/homebrew-bjpa/releases/download/0.1.1/BJPA-0.1.1-Darwin.tar.gz",
+  url "https://github.com/TorstenRobitzki/homebrew-bjpa/releases/download/0.1.1/BJPA-0.1.1-Darwin.tar.gz"
   sha256 "41acf6e10f8b28d67f962ee3977d09465152ee6b2a7eb7008ca41fe80f471dd6"
 
   def install
