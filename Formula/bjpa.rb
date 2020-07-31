a87ca8bb09a87885b131877be76cb9bfeb27cc31
Torsten Robitzki <Torsten@Robitzki.de>
2020-07-31 17:50:24 +0200

    new version: v0.3.1

--- Formula/bjpa.rb
+++ Formula/bjpa.rb
@@ -3,8 +3,8 @@ require 'fileutils'
 class Bjpa < Formula
   desc "Tools for BJPA"
   homepage "http://Torrox.de"
-  url "https://github.com/TorstenRobitzki/homebrew-bjpa/releases/download/v0.3.0/BJPA-0.3.0-Darwin.tar.gz"
-  sha256 "43a47ec4681142085053fd92b68315128b1531900626808dae36e082e561c392"
+  url "https://github.com/TorstenRobitzki/homebrew-bjpa/releases/download/v0.3.1/BJPA-0.3.1-Darwin.tar.gz"
+  sha256 "3da281051eb6deaf4b01ce4a42327d742729658cbb3c9bdff97c08a021e4778d"
 
   def install
     FileUtils.cp_r ".", "#{prefix}"
